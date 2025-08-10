5ec9d34881828a1ef3fda4c9a6e0e06b04535407
Nazrul Ali.N <nazrulali555@gmail.com>
2025-08-10 19:32:12 +0530

    signin correction

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
             <p className="text-2xl font-medium m-auto">
                 <span className="text-red-500">User</span> {state === "login" ? "Login" : "Sign Up"}
             </p>
-            {state === "register" && (
+            {state === "signin" && (
                 <div className="w-full">
                     <p>Name</p>
                     <input onChange={(e) => setName(e.target.value)} value={name} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-red-500" type="text" required />
@@ -51,13 +51,13 @@ const Login = () => {
                 <p>Password</p>
                 <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-red-500" type="password" required />
             </div>
-            {state === "register" ? (
+            {state === "signin" ? (
                 <p>
                     Already have account? <span onClick={() => setState("login")} className="text-red-500 cursor-pointer">click here</span>
                 </p>
             ) : (
                 <p>
-                    Create an account? <span onClick={() => setState("register")} className="text-red-500 cursor-pointer">click here</span>
+                    Create an account? <span onClick={() => setState("signin")} className="text-red-500 cursor-pointer">click here</span>
                 </p>
             )}
             <button onClick={handleClick} className="bg-red-500 hover:bg-red-600 transition-all text-white w-full py-2 rounded-md cursor-pointer">
