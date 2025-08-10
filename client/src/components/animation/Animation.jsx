1e32b567d14ccbb59b9b1685b989c49b04af715b
Nazrul Ali.N <nazrulali555@gmail.com>
2025-08-10 19:15:32 +0530

    sound

--- client/src/components/animation/Animation.jsx
+++ client/src/components/animation/Animation.jsx
@@ -1,11 +1,18 @@
 import Lottie from 'lottie-react'
 import Success from './Success.json'
+import SuccessSound from '../Sounds/Success.mp3'
+import { useEffect } from 'react'
 
 export const SuccessAnimation = () =>{
+    useEffect(() => {
+        const audio = new Audio(SuccessSound)
+        audio.play()
+    }, [])
     return(
         <div className='fixed flex inset-0 z-50 justify-center items-center bg-black/50 backdrop-blur-sm'>
             <div className='h-35 md:h-40 flex'>
                 <Lottie animationData={Success} loop={false} />
+                
             </div>
             
         </div>
