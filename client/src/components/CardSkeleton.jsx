d35d5ac8399da25db714f0d3d082f6f2a71f1ebb
Nazrul Ali.N <nazrulali555@gmail.com>
2025-08-10 19:36:13 +0530

    skellh

--- client/src/components/CardSkeleton.jsx
+++ client/src/components/CardSkeleton.jsx
@@ -4,7 +4,7 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
         <div className='relative h-auto w-38 md:h-110 md:w-70 border border-gray-500 rounded-lg hover:opacity-90'>
             <SkeletonTheme baseColor="#dcdcdc" highlightColor="#f5f5f5">
                 <div className='flex flex-col w-full'>
-                <div className='h-55 md:h-75 md:p-2 p-3'>
+                <div className='h-47 md:h-75 md:p-2 p-3'>
                      <Skeleton height='100%' />    
                 </div>
            
